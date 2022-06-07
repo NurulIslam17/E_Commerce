6a5e15af67cedbcd22d4286307b2107d4d63b0af
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-07 22:59:46 +0600

    Creating user and Admin Login and Register System

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeComtroller;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::get("/redirect",[HomeComtroller::class,"redirect"]);
